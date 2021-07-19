998264041b429446e781acae6177a1483c83ab97
Aslam <aslam1qqqq@gmail.com>
2021-07-19 12:14:46 +0530

    Hidden credentials

--- EmailApp/src/main/java/org/email/project/EmailApp/App.java
+++ EmailApp/src/main/java/org/email/project/EmailApp/App.java
@@ -1,32 +1,17 @@
 package org.email.project.EmailApp;
 
-import java.util.Iterator;
-
 import org.email.project.EmailApp.localdb.CollectionData;
-import org.email.project.EmailApp.reader.TextFileReader;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class App {
 
-	private static final String path = System.getProperty("user.dir")
-			+ "//src/main/java/org/email/project/EmailApp/localdb/";
-	private static final String filename = path + "email4.txt";
-
 	public static void main(String... args) throws Exception {
 
 //		BulkEmailController bController = new BulkEmailController("sample@gmail.com","pass****");
-
-		BulkEmailController bController = new BulkEmailController("aslamhit21009@gmail.com","hit21009");
-		
-
-//		Iterator<String> iter = CollectionData.get().iterator();
-//		int i = 1;
-//		while (iter.hasNext()) {
-//			System.out.println(i++ +" "+iter.next());
-//		}
+		BulkEmailController bController = new BulkEmailController(Dotenv.load().get("MY_ACCOUNT_EMAIL"),
+													Dotenv.load().get("MY_ACCOUNT_PASSWORD"));
 		
 		bController.sendBulkMail(CollectionData.get());
-		
 	}
 }
